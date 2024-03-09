08d1da5fea729b6cd574921fb379d9fa04406e4a
Dennis Moes <dennismoes@me.com>
2024-03-09 13:16:31 +0100

    Added a few important functions in the BaseController

--- src/main/java/nl/vu/cs/softwaredesign/app/Controllers/BaseController.java
+++ src/main/java/nl/vu/cs/softwaredesign/app/Controllers/BaseController.java
@@ -1,9 +1,12 @@
 package nl.vu.cs.softwaredesign.app.Controllers;
 
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -29,5 +32,61 @@ public abstract class BaseController {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    protected void openNewWindow(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/" + fxmlPath));
+            Parent root = loader.load();
+
+            Stage newStage = new Stage();
+            BaseController newController = loader.getController();
+            newController.setStage(newStage);
+
+            Scene scene = new Scene(root);
+            newStage.setScene(scene);
+            newStage.initModality(Modality.WINDOW_MODAL);
+            newStage.initOwner(stage);
+            newStage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    protected void closeCurrentWindow() {
+        stage.close();
+    }
+
+    protected void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    protected void showConfirmationDialog(String title, String message, Runnable yesAction, Runnable noAction) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+
+        ButtonType yesButton = new ButtonType("Yes", ButtonBar.ButtonData.YES);
+        ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.NO);
+        ButtonType cancelButton = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(yesButton, noButton, cancelButton);
+
+        alert.showAndWait().ifPresent(response -> {
+            if (response == yesButton) {
+                if (yesAction != null) {
+                    yesAction.run();
+                }
+            } else if (response == noButton) {
+                if (noAction != null) {
+                    noAction.run();
+                }
+            }
+        });
     }
 }
--- src/main/java/nl/vu/cs/softwaredesign/app/Controllers/SettingsPageController.java
+++ src/main/java/nl/vu/cs/softwaredesign/app/Controllers/SettingsPageController.java
@@ -1,4 +1,8 @@
 package nl.vu.cs.softwaredesign.app.Controllers;
 
+import javafx.fxml.FXML;
+
 public class SettingsPageController extends BaseController {
+    @FXML
+    public void initialize() { }
 }
