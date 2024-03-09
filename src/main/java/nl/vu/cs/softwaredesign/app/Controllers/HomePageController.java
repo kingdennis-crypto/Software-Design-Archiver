4b5d47f1c308445e496e7d520e1afee218288875
Dennis Moes <dennismoes@me.com>
2024-03-09 15:24:13 +0100

    Revamped the homepage with a folder chooser and treeview to display the selected files

--- src/main/java/nl/vu/cs/softwaredesign/app/Controllers/HomePageController.java
+++ src/main/java/nl/vu/cs/softwaredesign/app/Controllers/HomePageController.java
@@ -1,24 +1,81 @@
 package nl.vu.cs.softwaredesign.app.Controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+import java.util.Objects;
 
 public class HomePageController extends BaseController {
     @FXML
-    public void initialize() { }
+    private TreeView treeViewTable;
 
-    public void showHomeAlert() {
-        showAlert("Home Alert", "Hello from the home screen");
-    }
+    private File selectedFolder;
 
-    public void showHomeDialog() {
-        Runnable yes = () -> System.out.println("Yes");
-        Runnable no = () -> System.out.println("No");
-
-        showConfirmationDialog("Home dialog", "This is a simple yes no field", yes, no);
+    @FXML
+    public void initialize() {
+        clearSelectedFolder();
     }
 
     public void openSettingsPage() {
-        openNewWindow("nl/vu/cs/softwaredesign/settings-view.fxml");
+        openNewWindow("settings-view.fxml");
+    }
+
+    public void openReportPage() {
+        openNewWindow("report-view.fxml");
+    }
+
+    public void openTestPage() {
+        openNewWindow("test-view.fxml");
+    }
+
+    public void clearSelectedFolder() {
+        this.selectedFolder = null;
+        TreeItem<String> rootItem = new TreeItem<>("No folder chosen");
+        rootItem.setExpanded(true);
+        treeViewTable.setRoot(rootItem);
+    }
+
+    private void makeTreeItem(TreeItem<String> treeItem, File rootFile) {
+        Objects.requireNonNull(rootFile.listFiles());
+
+        for (File file : Objects.requireNonNull(rootFile.listFiles())) {
+            if (file.isFile()) {
+                TreeItem<String> fileItem = new TreeItem<>(file.getName());
+                treeItem.getChildren().add(fileItem);
+            } else {
+                TreeItem<String> nestedItem = new TreeItem<>(file.getName());
+                makeTreeItem(nestedItem, file);
+                treeItem.getChildren().add(nestedItem);
+            }
+        }
+
+    }
+
+    public void chooseFolder() {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select folder");
+
+        selectedFolder = directoryChooser.showDialog(stage);
+
+        if (selectedFolder != null) {
+            TreeItem<String> folderItem = new TreeItem<>(selectedFolder.getName());
+            makeTreeItem(folderItem, selectedFolder);
+            treeViewTable.setRoot(folderItem);
+        }
+    }
+
+    public void chooseArchive() {
+        System.out.println("Choosing archive");
+    }
+
+    public void deArchiveSelection() {
+        System.out.println("De archiving the selection");
+    }
+
+    public void archiveSelection() {
+        System.out.println("Archiving the selection");
     }
 }
