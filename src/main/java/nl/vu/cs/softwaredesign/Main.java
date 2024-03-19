e67dee0bd6f2bac73e54b9c68e0cb39578467f76
Dennis Moes <dennismoes@me.com>
2024-03-19 15:57:59 +0100

    Fixed the bug where the JAR opening didn't work. Now it does :)

--- src/main/java/nl/vu/cs/softwaredesign/Main.java
+++ src/main/java/nl/vu/cs/softwaredesign/Main.java
@@ -1,28 +1,7 @@
 package nl.vu.cs.softwaredesign;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import nl.vu.cs.softwaredesign.lib.Handlers.ConfigurationHandler;
-
-import java.io.IOException;
-
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        // Initialize singletons
-        ConfigurationHandler.getInstance();
-
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("home-view.fxml"));
-        Scene scene = new Scene(loader.load());
-
-        stage.setTitle("Archiver");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void main (String[] args){
-        launch();
+public class Main {
+    public static void main (String[] args) {
+        JavaFxApplication.main(args);
     }
 }
