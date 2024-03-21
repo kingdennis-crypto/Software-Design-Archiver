c09dce85977255765327d9faa124be5ca8fa5a97
Simon Vriesema <simon.vriesema@ah.nl>
2024-03-21 19:43:26 +0100

    Made the Status.java Enum and the IProgressListener.java Interface

---
+++ src/main/java/nl/vu/cs/softwaredesign/lib/enumerations/Status.java
@@ -0,0 +1,12 @@
+package nl.vu.cs.softwaredesign.lib.enumerations;
+
+/**
+ * Status of the progress process
+ * COMPILING: the process is still running
+ * FINISHED: the process has finished
+ */
+public enum Status {
+    COMPILING,
+    FINISHED
+}
+
