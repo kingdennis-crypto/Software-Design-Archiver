d9a44fa93c03b8a996517eb98e5c37ee98848d7b
Dennis Moes <dennismoes@me.com>
2024-03-22 19:31:07 +0100

    Rewrote the encryption algorithm to enable including metadata in the file and to either separately read or with the decrypted content

--- src/main/java/nl/vu/cs/softwaredesign/lib/Handlers/EncryptionHandler.java
+++ src/main/java/nl/vu/cs/softwaredesign/lib/Handlers/EncryptionHandler.java
@@ -1,11 +1,9 @@
 package nl.vu.cs.softwaredesign.lib.Handlers;
 
 import nl.vu.cs.softwaredesign.lib.Models.FileArchive;
+import nl.vu.cs.softwaredesign.lib.Models.KeyProperties;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.SecretKey;
+import javax.crypto.*;
 import javax.crypto.spec.GCMParameterSpec;
 import java.io.*;
 import java.util.Arrays;
@@ -16,16 +14,17 @@ public class EncryptionHandler {
     public EncryptionHandler() { }
 
     /**
-     * Encrypts a file using AES/GCM/NoPadding algorithm.
-     *
-     * @param path   The path to the file to be encrypted.
+     * Encrypts a file using AES/GCM/NoPadding algorithm and stores the metadata in plain text.
+     * @param path The path to the file to be encrypted.
      * @param secret The secret key used for encryption.
-     * @param iv     The initialization vector for encryption
+     * @param iv The initialization vector for encryption.
+     * @param metadata The metadata to be associated with the encrypted file.
+     * @throws FileNotFoundException If the specified file does not exist.
      */
     public static void encryptFile(String path, SecretKey secret, byte[] iv, Map<String, String> metadata) throws FileNotFoundException {
         try {
             File inputFile = new File(path);
-            File outputFile = new File(path);
+            File outputFile = new File(path + ".enc");
 
             if (!inputFile.exists())
                 throw new FileNotFoundException(String.format("File %s does not exist", inputFile.getName()));
@@ -34,16 +33,31 @@ public class EncryptionHandler {
             cipher.init(Cipher.ENCRYPT_MODE, secret, new GCMParameterSpec(128, iv));
 
             try (FileInputStream inputStream = new FileInputStream(inputFile);
-                 FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+                    FileOutputStream outputStream = new FileOutputStream(outputFile)) {
 
-                byte[] metadataOutput = cipher.update(metadata.toString().getBytes(), 0, metadata.toString().getBytes().length);
-                outputStream.write(metadataOutput);
+                // Write metadata to the output file
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                objectOutputStream.writeObject(metadata);
 
-                ByteArrayOutputStream encryptedContent = EncryptionHandler.fileToByteArray(inputStream, cipher, new ByteArrayOutputStream());
-                outputStream.write(encryptedContent.toByteArray());
+                // Encrypt file contents
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    byte[] encryptedBytes = cipher.update(buffer, 0, bytesRead);
+                    if (encryptedBytes != null)
+                        outputStream.write(encryptedBytes);
+                }
+
+                byte[] finalBytes = cipher.doFinal();
+                if (finalBytes != null)
+                    outputStream.write(finalBytes);
             }
 
-        } catch (FileNotFoundException ex) {
+            // Move the encrypted file to the input file
+            if (!outputFile.renameTo(inputFile))
+                throw new IOException("Failed to rename the encrypted file to the input file.");
+
+        } catch(FileNotFoundException ex) {
             throw ex;
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -51,11 +65,13 @@ public class EncryptionHandler {
     }
 
     /**
-     * Decrypts a file using the AES/GCM/NoPadding algorithm.
-     *
-     * @param path   The path to the file to be encrypted.
+     * Decrypts a file encrypted with AES/GCM/NoPadding algorithm and extracts the associated metadata.
+     * The decrypted content will be written back to the input file
+     * @param path The path to the file to be decrypted.
      * @param secret The secret key used for decryption.
-     * @param iv     The initialization vector for decryption.
+     * @param iv The initialization vector for decryption.
+     * @return A {@link FileArchive} Object containing the decrypted file and its associated metadata.
+     * @throws FileNotFoundException If the specified file does not exist/
      */
     public static FileArchive decryptFile(String path, SecretKey secret, byte[] iv) throws FileNotFoundException {
         try {
@@ -63,40 +79,36 @@ public class EncryptionHandler {
             cipher.init(Cipher.DECRYPT_MODE, secret, new GCMParameterSpec(128, iv));
 
             File inputFile = new File(path);
-            ByteArrayOutputStream encryptedContent = new ByteArrayOutputStream();
+            Map<String, String> metadata;
 
             if (!inputFile.exists())
                 throw new FileNotFoundException(String.format("File %s does not exist", inputFile.getName()));
 
-            try (InputStream inputStream = new FileInputStream(inputFile)) {
-                EncryptionHandler.fileToByteArray(inputStream, cipher, encryptedContent);
-            }
+            try (FileInputStream inputStream = new FileInputStream(inputFile)) {
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+                metadata = (Map<String, String>) objectInputStream.readObject();
 
-            int position = 0;
-            StringBuilder metadataString = new StringBuilder();
+                ByteArrayOutputStream decryptedContent = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
 
-            for (int i = 0; i < encryptedContent.size(); i++) {
-                metadataString.append((char) encryptedContent.toByteArray()[i]);
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    byte[] decryptedBytes = cipher.update(buffer, 0, bytesRead);
 
-                if ((char) encryptedContent.toByteArray()[i] == '}') {
-                    position = i + 1;
-                    break;
+                    if (decryptedBytes != null)
+                        decryptedContent.write(decryptedBytes);
                 }
+
+                byte[] finalBytes = cipher.doFinal();
+                if (finalBytes != null)
+                    decryptedContent.write(finalBytes);
+
+                try (FileOutputStream outputStream = new FileOutputStream(inputFile)) {
+                    outputStream.write(decryptedContent.toByteArray());
+                }
+
+                return new FileArchive(inputFile, metadata);
             }
-
-            Map<String, String> metadata = Arrays.stream(metadataString.substring(1, metadataString.length() - 1).split(", "))
-                    .map(pair -> pair.split("="))
-                    .collect(HashMap::new, (map, pair) -> map.put(pair[0], pair[1]), HashMap::putAll);
-
-            try (OutputStream outputStream = new FileOutputStream(inputFile.getAbsoluteFile())) {
-                byte[] fileContent = Arrays.copyOfRange(encryptedContent.toByteArray(), position, encryptedContent.size());
-                outputStream.write(fileContent);
-
-                if (!inputFile.delete())
-                    System.out.println("Something went wrong trying to delete this file");
-            }
-
-            return new FileArchive(inputFile, metadata);
         } catch (FileNotFoundException ex) {
             throw ex;
         } catch (Exception ex) {
@@ -106,35 +118,17 @@ public class EncryptionHandler {
     }
 
     /**
-     * Converts data from an {@link InputStream} to a byte array using the provided {@link Cipher}.
-     *
-     * @param inputStream   The InputStream containing the input data.
-     * @param cipher        The Cipher instance used for transformation.
-     * @param outputStream  The ByteArrayOutputStream for the transformed data.
-     * @return              The byte array containing the transformed data.
-     * @throws IOException                  If an I/O error occurs during reading from the InputStream.
-     * @throws IllegalBlockSizeException    If the block size of the Cipher is invalid during transformation.
-     * @throws BadPaddingException          If padding is expected but not properly applied during transformation.
+     * Reads metadata from a file without decrypting its content.
+     * @param path The path to the encrypted file.
+     * @return A map containing the metadata extracted from the file.
      */
-    private static ByteArrayOutputStream fileToByteArray(InputStream inputStream, Cipher cipher, ByteArrayOutputStream outputStream)
-            throws IOException, IllegalBlockSizeException, BadPaddingException {
-        int bufferSize = 1024;
-        byte[] buffer = new byte[bufferSize];
-        int bytesRead;
-
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            byte[] output = cipher.update(buffer, 0, bytesRead);
-
-            if (output != null)
-                outputStream.write(output);
+    public static Map<String, String> readMetadataFromFile(String path) {
+        try (FileInputStream inputStream = new FileInputStream(path);
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
+            return (Map<String, String>) objectInputStream.readObject();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
         }
-
-        byte[] outputBytes = cipher.doFinal();
-
-        if (outputBytes != null) {
-            outputStream.write(outputBytes);
-        }
-
-        return outputStream;
     }
 }
