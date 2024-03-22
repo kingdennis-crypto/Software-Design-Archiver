34b03d81deb2db7bb87e122da02688928a8e5397
Simon Vriesema <simon.vriesema@ah.nl>
2024-03-22 14:28:45 +0100

    Created the ProgressLogger.java class

---
+++ src/main/java/nl/vu/cs/softwaredesign/lib/Singleton/ProgressLogger.java
@@ -0,0 +1,63 @@
+package nl.vu.cs.softwaredesign.lib.Singleton;
+
+import nl.vu.cs.softwaredesign.lib.enumerations.Status;
+import nl.vu.cs.softwaredesign.lib.interfaces.IProgressListener;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * ProgressLogger class implements the IProgressListener interface and provides
+ * functionality to log progress updates to a logfile.
+ */
+public class ProgressLogger implements IProgressListener {
+    private static ProgressLogger instance;
+    private File logFile;
+
+    /**
+     * Private constructor to prevent instantiation from outside the class.
+     * Initializes the log file.
+     */
+    private ProgressLogger() {
+        logFile = new File("logfile.txt");
+    }
+
+    /**
+     * Retrieves the singleton instance of ProgressLogger.
+     * If instance is null, creates a new instance and also creates the logfile
+     * @return The singleton instance of ProgressLogger.
+     */
+    public static ProgressLogger getInstance() {
+        if (instance == null) instance = new ProgressLogger();
+        return instance;
+    }
+
+    /**
+     * Updates the progress status and writes it to the log file.
+     * @param status The status to be logged.
+     */
+    @Override
+    public void update(Status status) {
+        try (FileWriter writer = new FileWriter(logFile, true)) {
+            writer.write(status.toString() + "\n");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * Updates the progress status for the existing file and writes it to the file.
+     * @param status The status to be logged.
+     * @param file The existing file to log the status to.
+     */
+    @Override
+    public void update(Status status, File file) {
+        try (FileWriter writer = new FileWriter(file, true)) {
+            writer.write(status.toString() + "\n");
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
+
